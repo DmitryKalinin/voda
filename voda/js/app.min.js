111924a7fcfccf9a3134ec9ccf1715df29a7e52e
Dmitry Kalinin <d.kalinin@labsales.ru>
2021-10-01 09:16:39 +0700

    Верстка роль администратор

--- voda/js/app.min.js
+++ voda/js/app.min.js
@@ -498,6 +498,18 @@ for(serchElemrnt of serchElemrnts){
 
 
 
+document.querySelector("body").addEventListener("click",(e)=>{
+        if (e.target.classList.contains("password-control")){
+            let newTarget = document.querySelector(`.${e.target.dataset.targetPassword}`);
+            if (newTarget.getAttribute('type') == 'password') {
+                e.target.classList.add('view');
+                newTarget.setAttribute('type', 'text');
+            } else {
+                e.target.classList.remove('view');
+                newTarget.setAttribute('type', 'password');
+            }
+        }  
+    })
 
 
 //slider
